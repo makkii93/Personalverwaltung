1bc389c7638202a1dfacfd37e58671b2cf5b53ab
mak <105160900+makkii93@users.noreply.github.com>
2023-11-21 15:00:52 +0100

    index.php

--- index.php
+++ index.php
@@ -7,97 +7,59 @@ if ($conn->connect_error) {
     die("Verbindung fehlgeschlagen: " . $conn->connect_error); // Bei Fehlschlagen der Verbindung: Fehlermeldung anzeigen und Skript beenden
 }
 
+// Überprüfen, ob das Formular zum Hinzufügen eines Mitarbeiters gesendet wurde
+if ($_SERVER["REQUEST_METHOD"] == "POST") { // Überprüfe, ob die Anfrage eine POST-Anfrage ist
+    // Überprüfe, ob die POST-Variablen existieren, bevor du darauf zugreifst
+    $name = isset($_POST['name']) ? $_POST['name'] : ''; // Den Wert von 'name' aus dem Formular lesen, wenn nicht vorhanden, leer lassen
+    $vorname = isset($_POST['vorname']) ? $_POST['vorname'] : ''; // Den Wert von 'vorname' aus dem Formular lesen, wenn nicht vorhanden, leer lassen
+    $email = isset($_POST['email']) ? $_POST['email'] : ''; // Den Wert von 'email' aus dem Formular lesen, wenn nicht vorhanden, leer lassen
+    $tel = isset($_POST['tel']) ? $_POST['tel'] : ''; // Den Wert von 'tel' aus dem Formular lesen, wenn nicht vorhanden, leer lassen
+    $position = isset($_POST['position']) ? $_POST['position'] : ''; // Den Wert von 'position' aus dem Formular lesen, wenn nicht vorhanden, leer lassen
+    $abteilung = isset($_POST['abteilung']) ? $_POST['abteilung'] : ''; // Den Wert von 'abteilung' aus dem Formular lesen, wenn nicht vorhanden, leer lassen
 
-
-// SQL-Befehl zum Einfügen eines neuen Mitarbeiters in die Datenbank
-if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['submit'])) {
-    // Überprüfen, ob die POST-Variablen existieren, bevor darauf zugegriffen wird
-    $name = isset($_POST['name']) ? $_POST['name'] : '';
-    $vorname = isset($_POST['vorname']) ? $_POST['vorname'] : '';
-    $email = isset($_POST['email']) ? $_POST['email'] : '';
-
-    // Überprüfen, ob der Mitarbeiter bereits existiert
-    $check_query = "SELECT * FROM Mitarbeiter WHERE name='$name' AND vorname='$vorname' AND email='$email'";
-    $result = $conn->query($check_query); // Abfrage in der Datenbank ausführen
+    // Überprüfe, ob der Mitarbeiter bereits existiert
+    $check_query = "SELECT * FROM Mitarbeiter WHERE name='$name' AND vorname='$vorname'";
+    $result = $conn->query($check_query); // Führe die Abfrage in der Datenbank aus
 
     if ($result->num_rows > 0) {
-        // Mitarbeiter existiert bereits, zeige eine entsprechende Meldung
-        echo "Der Mitarbeiter existiert bereits in der Datenbank.";
+        // Mitarbeiter existiert bereits
+        echo '<script>document.getElementById("message").innerHTML = "Dieser Mitarbeiter ist bereits in der Datenbank"; document.getElementById("message").style.display = "block";</script>';
     } else {
         // Mitarbeiter existiert nicht, füge ihn hinzu
-        $insert_query = "INSERT INTO Mitarbeiter (name, vorname, email) VALUES ('$name', '$vorname', '$email')";
+        $insert_query = "INSERT INTO Mitarbeiter (name, vorname, email, tel, position, abteilung) VALUES ('$name', '$vorname', '$email', '$tel', '$position', '$abteilung')";
 
         if ($conn->query($insert_query) === TRUE) {
-            // Erfolgreich eingefügt, zeige eine Bestätigung
-            echo "Mitarbeiter erfolgreich angelegt.";
+            echo '<script>document.getElementById("message").innerHTML = "Neuer Mitarbeiter erfolgreich angelegt"; document.getElementById("message").style.display = "block";</script>';
         } else {
-            // Fehler beim Einfügen, zeige Fehlermeldung
-            echo "Fehler beim Anlegen des Mitarbeiters: " . $conn->error;
+            echo '<script>document.getElementById("message").innerHTML = "Fehler beim Anlegen des Mitarbeiters: ' . $conn->error . '"; document.getElementById("message").style.display = "block";</script>';
         }
     }
 }
-
 // Bearbeiten eines Mitarbeiters anhand der Mitarbeiter-ID
-if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['submit'])) {
-    // Überprüfen, ob die POST-Variablen existieren, bevor darauf zugegriffen wird
-    $mitarbeiter_id = isset($_POST['mitarbeiter_id']) ? $_POST['mitarbeiter_id'] : '';
-
-    // Hier könntest du weitere Felder zum Aktualisieren einfügen
-    $name = isset($_POST['name']) ? $_POST['name'] : '';
-    $vorname = isset($_POST['vorname']) ? $_POST['vorname'] : '';
-
-
-    // Annahme: Aktualisierung des Namens basierend auf der Mitarbeiter-ID
-    $update_query = "UPDATE Mitarbeiter SET name = 'Neuer Name' WHERE mitarbeiter_id = $mitarbeiter_id";
-
-    if ($conn->query($update_query) === TRUE) {
-        // Erfolgreich aktualisiert, zeige Bestätigung
-        echo "Erfolgreich in der Datenbank aktualisiert!";
-    } else {
-        // Fehler beim Aktualisieren, zeige Fehlermeldung
-        echo "Fehler beim Aktualisieren in der Datenbank: " . $conn->error;
-    }
+if (isset($_POST['edit'])) {
+    // Hier ergänze den Code zum Bearbeiten eines Mitarbeiters
 }
 
 // Suchen nach einem Mitarbeiter
-if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['submit'])) {
-    // Überprüfen, ob die POST-Variablen existieren, bevor darauf zugegriffen wird
-    $suchbegriff = isset($_POST['suchbegriff']) ? $_POST['suchbegriff'] : '';
+if (isset($_POST['suchbegriff'])) {
+    $search_query = $_POST['suchbegriff'];
+    // SQL-Befehl zum Suchen nach einem Mitarbeiter in der Datenbank
+    $search_result = $conn->query("SELECT * FROM Mitarbeiter WHERE name LIKE '%$search_query%' OR vorname LIKE '%$search_query%'");
 
-    // SQL-Abfrage zum Suchen des Mitarbeiters in der Datenbank
-    $search_query = "SELECT * FROM Mitarbeiter WHERE name='$suchbegriff'";
-    $result = $conn->query($search_query);
-
-    // Ergebnis der Suche verarbeiten
-    if ($result->num_rows > 0) {
-        // Mitarbeiter gefunden, zeige die Informationen an oder bearbeite sie entsprechend deiner Anforderungen
-        $row = $result->fetch_assoc();
-        echo "Der Mitarbeiter mit dem Namen " . $row['name'] . " wurde gefunden.";
+    if ($search_result->num_rows > 0) {
+        // Wenn der Mitarbeiter gefunden wurde
+        while ($row = $search_result->fetch_assoc()) {
+            echo "Der Mitarbeiter mit dem Namen " . $row['suchbegriff'] . " wurde gefunden.";
+        }
     } else {
-        // Mitarbeiter nicht gefunden, gib eine Nachricht aus, um einen neuen Mitarbeiter anzulegen
-        echo "Der Mitarbeiter mit dem Namen " . $suchbegriff . " befindet sich nicht in unserer Datenbank. Bitte legen Sie einen neuen an.";
+        // Wenn der Mitarbeiter nicht gefunden wurde
+        echo "Der Mitarbeiter ist nicht in der Datenbank vorhanden.";
     }
 }
 
 // Drucken einer Mitarbeiterliste
-// Überprüfen, ob das Formular zum Drucken einer Mitarbeiterliste gesendet wurde
-if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['submit'])) {
-    // SQL-Abfrage, um alle Mitarbeiter aus der Datenbank abzurufen
-    $query = "SELECT * FROM Mitarbeiter";
-    $result = $conn->query($query);
-
-    // Ergebnis der Abfrage verarbeiten und Mitarbeiterliste ausgeben
-    if ($result->num_rows > 0) {
-        echo "<h3>Mitarbeiterliste:</h3>";
-        while ($row = $result->fetch_assoc()) {
-            // Hier kannst du die Informationen der Mitarbeiter anzeigen
-            echo "Name: " . $row['name'] . ", Vorname: " . $row['vorname'] . ", Email: " . $row['email'] . "<br>";
-            // ... Weitere Informationen je nach Bedarf ausgeben
-        }
-        echo "<p>Mitarbeiterliste wird ausgedruckt.</p>";
-    } else {
-        echo "Keine Mitarbeiter gefunden.";
-    }
+if (isset($_POST['print'])) {
+    // Code für das Drucken einer Mitarbeiterliste
 }
 
 // Datenbankverbindung schließen
